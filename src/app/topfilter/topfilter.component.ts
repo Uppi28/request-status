e4fbaf34d19e78db1a9a7d728f5ff4909422232d
Uppi28 <upendrabr.93@gmail.com>
2020-05-12 18:18:02 +0530

    Search styled

--- src/app/topfilter/topfilter.component.ts
+++ src/app/topfilter/topfilter.component.ts
@@ -9,37 +9,37 @@ import { MatOption } from '@angular/material/core';
 })
 export class TopfilterComponent implements OnInit {
 
-  @Input() filters: {key: number, name: string}[];
-  @Input() userProfile: [{key: number, name: string}];
+  @Input() filters: { key: number, name: string }[];
+  @Input() userProfile: [{ key: number, name: string }];
   @Input() title: string;
 
 
   searchUserForm: FormGroup;
   selected: number[];
-  initialFilters: {key: number, name: string}[];
+  initialFilters: { key: number, name: string }[];
 
   @ViewChild('allSelected') private allSelected: MatOption;
 
-  constructor(private fb: FormBuilder){}
+  constructor(private fb: FormBuilder) { }
 
   onKey(value) {
-    if(value === ''){
+    if (value === '') {
       this.search('')
     } else {
       this.filters = this.search(value);
     }
   }
-    
+
   search(value: string) {
-      this.filters = [...this.initialFilters];
-      let filter = value.toLowerCase();      
-      return this.initialFilters.filter(option => option.name.toLowerCase().includes(filter));
+    this.filters = [...this.initialFilters];
+    let filter = value.toLowerCase();
+    return this.initialFilters.filter(option => option.name.toLowerCase().includes(filter));
   }
 
   ngOnInit() {
     this.initialFilters = [...this.filters];
-    
-    this.selected = this.userProfile.map((d)=>d.key);
+
+    this.selected = this.userProfile.map((d) => d.key);
 
     this.searchUserForm = this.fb.group({
       userType: new FormControl('')
@@ -48,15 +48,15 @@ export class TopfilterComponent implements OnInit {
   compareWithFunc(a, b) {
     return a.name === b.name;
   }
-  togglePerOne(all){ 
-    if (this.allSelected.selected) {  
+  togglePerOne(all) {
+    if (this.allSelected.selected) {
       this.allSelected.deselect();
       return false;
-  }
-  if(this.searchUserForm.controls.userType.value.length==this.filters.length)
-    this.allSelected.select();
+    }
+    if (this.searchUserForm.controls.userType.value.length == this.filters.length)
+      this.allSelected.select();
 
-}
+  }
   toggleAllSelection() {
     if (this.allSelected.selected) {
       this.searchUserForm.controls.userType
